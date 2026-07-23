9dc35a86142381f67e4734878f57d4f68fa03527
Virajpotdar10 <virajpotdar4@gmail.com>
2026-07-23 14:16:49 +0530

    updated:Experiment2

--- Experiment_2.py
+++ Experiment_2.py
@@ -206,3 +206,191 @@
 #     for j in range(i):
 #         print(j+1, end=" ")
 #     print()
+
+
+
+
+# WHILE LOOP PROGRAMS
+
+# 1. Print all natural numbers up to n
+# print("1. Natural Numbers")
+# n = int(input("Enter n: "))
+# i = 1
+# while i <= n:
+#     print(i)
+#     i += 1
+
+# 2. Print all even numbers up to n
+# print("\n2. Even Numbers")
+# n = int(input("Enter n: "))
+# i = 2
+# while i <= n:
+#     print(i)
+#     i += 2
+
+# 3. Print all odd numbers up to n
+# print("\n3. Odd Numbers")
+# n = int(input("Enter n: "))
+# i = 1
+# while i <= n:
+#     print(i)
+#     i += 2
+
+# 4. Sum of natural numbers up to n
+# print("\n4. Sum of Natural Numbers")
+# n = int(input("Enter n: "))
+# i = 1
+# sum = 0
+# while i <= n:
+#     sum += i
+#     i += 1
+# print("Sum =", sum)
+
+# 5. Sum of odd numbers up to n
+# print("\n5. Sum of Odd Numbers")
+# n = int(input("Enter n: "))
+# i = 1
+# sum = 0
+# while i <= n:
+#     sum += i
+#     i += 2
+# print("Sum =", sum)
+
+# 6. Sum of even numbers up to n
+# print("\n6. Sum of Even Numbers")
+# n = int(input("Enter n: "))
+# i = 2
+# sum = 0
+# while i <= n:
+#     sum += i
+#     i += 2
+# print("Sum =", sum)
+
+# 7. Print natural numbers in reverse order
+# print("\n7. Natural Numbers in Reverse")
+# n = int(input("Enter n: "))
+# while n >= 1:
+#     print(n)
+#     n -= 1
+
+# 8. Fibonacci series up to n terms
+# print("\n8. Fibonacci Series")
+# n = int(input("Enter number of terms: "))
+# a = 0
+# b = 1
+# count = 1
+# while count <= n:
+#     print(a, end=" ")
+#     c = a + b
+#     a = b
+#     b = c
+#     count += 1
+# print()
+
+# 9. Factorial of a number
+# print("\n9. Factorial")
+# n = int(input("Enter number: "))
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+# print("Factorial =", fact)
+
+
+# 10. Check Prime Number
+# print("\n10. Prime Number Check")
+# n = int(input("Enter number: "))
+# i = 2
+# prime = True
+
+# if n <= 1:
+#     prime = False
+# else:
+#     while i < n:
+#         if n % i == 0:
+#             prime = False
+#             break
+#         i += 1
+
+# if prime:
+#     print("Prime Number")
+# else:
+#     print("Not a Prime Number")
+
+# 11. Sum of digits
+# print("\n11. Sum of Digits")
+# n = int(input("Enter number: "))
+# temp = n
+# sum = 0
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit
+#     temp //= 10
+# print("Sum of digits =", sum)
+
+# 12. Palindrome Number
+# print("\n12. Palindrome Check")
+# n = int(input("Enter number: "))
+# temp = n
+# rev = 0
+
+# while temp > 0:
+#     digit = temp % 10
+#     rev = rev * 10 + digit
+#     temp //= 10
+
+# if n == rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# 13. Reverse a Number
+# print("\n13. Reverse Number")
+# n = int(input("Enter number: "))
+# rev = 0
+# while n > 0:
+#     digit = n % 10
+#     rev = rev * 10 + digit
+#     n //= 10
+# print("Reverse =", rev)
+
+
+# 14. Multiplication Table
+# print("\n14. Multiplication Table")
+# n = int(input("Enter number: "))
+# i = 1
+# while i <= 10:
+#     print(n, "x", i, "=", n * i)
+#     i += 1
+
+
+# 15. Largest of n Numbers
+# print("\n15. Largest of n Numbers")
+# count = int(input("How many numbers? "))
+# i = 1
+# largest = None
+
+# while i <= count:
+#     num = int(input("Enter number: "))
+#     if largest is None or num > largest:
+#         largest = num
+#     i += 1
+
+# print("Largest =", largest)
+
+
+# 16. Smallest of n Numbers
+# print("\n16. Smallest of n Numbers")
+# count = int(input("How many numbers? "))
+# i = 1
+# smallest = None
+
+# while i <= count:
+#     num = int(input("Enter number: "))
+#     if smallest is None or num < smallest:
+#         smallest = num
+#     i += 1
+
+# print("Smallest =", smallest)
