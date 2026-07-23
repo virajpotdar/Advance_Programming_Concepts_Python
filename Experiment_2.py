c939ed31f1df96b90b09899c7c2377357e2d214e
Virajpotdar10 <virajpotdar4@gmail.com>
2026-07-23 13:47:03 +0530

    Added for while and if

--- Experiment_2.py
+++ Experiment_2.py
@@ -201,8 +201,8 @@
 
 
 
-n = int(input("Enter a number: "))
-for i in range(n,1):
-    for j in range(i):
-        print(j+1, end=" ")
-    print()
+# n = int(input("Enter a number: "))
+# for i in range(n,1):
+#     for j in range(i):
+#         print(j+1, end=" ")
+#     print()
